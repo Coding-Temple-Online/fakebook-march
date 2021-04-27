a08a1d80a6ba6b763016bb0ee27381fe224210a7
derekcodingtemple <derekh@codingtemple.com>
2021-04-27 11:41:19 -0500

    added User authentication

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
 from app import app, db
-from app.models import Post
+from app.models import Post, User
 
 @app.shell_context_processor
 def make_shell_context():
     # ALWAYS HAVE TO RETURN A PYTHON DICTIONARY FROM CONTEXT
     return {
         'db': db,
-        'Post': Post
+        'Post': Post,
+        'User': User
     }
